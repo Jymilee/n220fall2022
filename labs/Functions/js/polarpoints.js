f8ce9c0cefbb558fc7ce2cb5f672dcf97d23fbd7
Jymilee <110702623+Jymilee@users.noreply.github.com>
2022-09-15 21:54:46 -0400

    updated index

--- labs/Functions/js/polarpoints.js
+++ labs/Functions/js/polarpoints.js
@@ -1,22 +1,26 @@
+/*Jymilee Wyndle 
+  Travis Faas
+  N220 Section 24603 
+  September 15, 2022 */
 
-function setup() {
-    createCanvas(800, 600);
+//Begin setup function
+function setup() { //Create setup function
+    createCanvas(800, 600); //set canvas size to be 800px by 600px
 }
 
+//Begin draw function
+function draw() { //Create draw function
+    translate(100, 100); //move starting point of circle to be at 100x and 100y
+    var res = polarPoint(60); //set variable to return of function polarPoint, set r value to 60
 
-function draw() {
-    translate(100, 100);
-    var res = polarPoint(60); 
-
-    ellipse(res.x, res.y, 10);
-
-
+    ellipse(res.x, res.y, 10); //create circle at return of object resx and object resy with a 10px radius
 }
 
-function polarPoint(r) {
-    var x = r * Math.sin(mouseX);
-    var y = r * Math.cos(mouseX);
+//Begin polarPoint function
+function polarPoint(r) { //create polarPoint function
+    var x = r * Math.sin(mouseX); //set variable x to equal parameter r time the sin of point of mouse x value
+    var y = r * Math.cos(mouseX); //set variable y to equal parameter r times the cosine of point of mouse x value
 
-    return createVector(x, y);
+    return createVector(x, y); //return the value of function and create a vector with parameters of coordinates
 
 }
--- labs/Functions/js/redremover.js
+++ labs/Functions/js/redremover.js
@@ -1,16 +1,23 @@
+/*Jymilee Wyndle
+  Travis Faas
+  N220 Section 24603
+  September 15, 2022 */
 
-function setup() {
-    createCanvas(800,600);
-    background (226, 224, 236);
+//Begin setup function
+function setup() { //create function setup
+    createCanvas(800,600); //set canvas size to 800px by 600px
+    background (226, 224, 236); //set background to pink
 }
 
-function draw() {
-   removeRed();
+//begin draw function
+function draw() { //create function draw
+   removeRed(); //invoke removeRed function
 }
 
-function removeRed (r, b, g) {
-    let noRed = color(170, 200, 150);
-    noRed.setRed(0);
-    fill(noRed);
-    circle(400, 300, 50, 50);
+//Create function to remove red from circle
+function removeRed (r, b, g) { //begin removeRed function
+    let noRed = color(170, 200, 150); //set variable noRed to equal a shade of green with red
+    noRed.setRed(0); //set variable noRed to set the red amount to zero
+    fill(noRed); //fill circle with variable noRed, removing the red from the shade of green
+    circle(400, 300, 50, 50); //draw circle to be at 400x by 300y with a radius of 50px
 }
